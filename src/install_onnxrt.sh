a347589f1cf4d6a55f178565097d5cf1dc0e6be4
Vito Spatafora <143433342+VitoSpatafora@users.noreply.github.com>
2025-12-07 23:45:50 -0500

    Delete src directory

--- src/install_onnxrt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-rm -rf onnxruntime
-wget https://github.com/microsoft/onnxruntime/releases/download/v1.17.3/onnxruntime-linux-x64-1.17.3.tgz
-tar -xzf onnxruntime-linux-x64-1.17.3.tgz
-mv onnxruntime-linux-x64-1.17.3 onnxruntime
